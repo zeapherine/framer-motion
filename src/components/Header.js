058499354f2cfc5d456f25c8e55daafd24788e1a
Zeapherine <zeapherine@gmail.com>
2021-06-04 23:45:56 +0530

    added svg animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,56 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
+const svgVariants = {
+	initial: {
+		rotate: -180,
+	},
+	visible: {
+		rotate: 0,
+		transition: {
+			duration: 1,
+		},
+	},
+};
+
+const pathVariants = {
+	initial: {
+		opacity: 0,
+		pathLength: 0,
+	},
+	visible: {
+		opacity: 1,
+		pathLength: 1,
+		transition: {
+			duration: 2,
+			ease: 'easeInOut',
+		},
+	},
+};
+
 const Header = () => {
 	return (
 		<header>
 			<div className='logo'>
-				<svg
+				<motion.svg
 					className='pizza-svg'
 					xmlns='http://www.w3.org/2000/svg'
 					viewBox='0 0 100 100'
+					variants={svgVariants}
+					initial='initial'
+					animate='visible'
 				>
-					<path
+					<motion.path
 						fill='none'
 						d='M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z'
+						variants={pathVariants}
 					/>
-					<path fill='none' d='M50 30 L50 -10 C50 -10 90 -10 90 30 Z' />
-				</svg>
+					<motion.path
+						fill='none'
+						d='M50 30 L50 -10 C50 -10 90 -10 90 30 Z'
+						variants={pathVariants}
+					/>
+				</motion.svg>
 			</div>
 			<motion.div
 				initial={{ y: -250 }}
