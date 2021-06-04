cfe8eeed319cdf8cd701002426ce2218903d520f
Zeapherine <zeapherine@gmail.com>
2021-06-04 23:56:38 +0530

    added loader with animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import Loader from './Loader';
 
 const buttonVariants = {
 	hover: {
@@ -51,6 +52,8 @@ const Home = () => {
 					Create Your Pizza
 				</motion.button>
 			</Link>
+
+			<Loader/>
 		</motion.div>
 	);
 };
