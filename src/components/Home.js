81d5528e124921b789da51bafc8f2f9472f020bc
Zeapherine <zeapherine@gmail.com>
2021-06-04 16:48:07 +0530

    added finite key frames for button

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const buttonVariants = {
+	hover: {
+		scale: [1, 1.1, 1, 1.1, 1, 1.1], //keyframes--- it will scale back and fort 1 - 1.1
+		textShadow: '0px 0px 8px rgb(255, 255, 255)',
+		boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+	},
+};
+
 const Home = () => {
 	return (
 		<motion.div
@@ -13,13 +21,7 @@ const Home = () => {
 			<h2>Welcome to Pizza Joint</h2>
 
 			<Link to='/base'>
-				<motion.button
-					whileHover={{
-						scale: 1.1,
-						textShadow: '0px 0px 8px rgb(255, 255, 255)',
-						boxShadow: '0px 0px 8px rgb(255, 255, 255)',
-					}}
-				>
+				<motion.button variants={buttonVariants} whileHover='hover'>
 					Create Your Pizza
 				</motion.button>
 			</Link>
