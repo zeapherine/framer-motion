ccf6ea9856110cc4bc4f424b301c040fc14af3df
Zeapherine <zeapherine@gmail.com>
2021-06-04 15:35:32 +0530

    variants added to Base.js

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,15 +2,55 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+/* Variants
+		1) it allows us to extract the object in outside object and reference to it.
+		2) it allows us to propagate variant changes through the dom
+			resulting in cleaner code.
+		3) it allows us to create timing relationship between children motion
+			and parent motion using TRANSITION Orchestration properties.
+
+*/
+
+const containerVariants = {
+	hidden: {
+		opacity: 0,
+		x: '100vw',
+	},
+	visible: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			type: 'spring',
+			delay: 0.5,
+		},
+	},
+};
+
+const nextVariants = {
+	hidden: {
+		x: '-100vw',
+	},
+	visible: {
+		x: 0,
+		transition: {
+			type: 'spring',
+			stiffness: 120,
+		},
+	},
+};
+
 const Base = ({ addBase, pizza }) => {
 	const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
 	return (
 		<motion.div
 			className='base container'
-			initial={{ x: '100vw' }}
-			animate={{ x: 0 }}
-			transition={{ type: 'spring', delay: 0.5 }}
+			// initial={{ x: '100vw' }}
+			// animate={{ x: 0 }}
+			// transition={{ type: 'spring', delay: 0.5 }} //Replaced with variants object.
+			variants={containerVariants}
+			initial='hidden' //this two lines can be inherited by children, eg: down below, div with class='next'
+			animate='visible' //this two lines can be inherited by children, eg: down below, div with class='next'
 		>
 			<h3>Step 1: Choose Your Base</h3>
 			<ul>
@@ -32,9 +72,9 @@ const Base = ({ addBase, pizza }) => {
 			{pizza.base && (
 				<motion.div
 					className='next'
-					initial={{ x: '-100vw' }}
-					animate={{ x: 0 }}
-					transition={{ type: 'spring', stiffness: 120 }}
+					variants={nextVariants}
+					// initial='hidden'			/* we don't need to include this two lines, it is getting these properties from parent.
+					// animate='visible'
 				>
 					<Link to='/toppings'>
 						<motion.button
