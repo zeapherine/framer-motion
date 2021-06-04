40058cda34f2fcb1dce9200e54ef621e930988cf
Zeapherine <zeapherine@gmail.com>
2021-06-05 01:05:39 +0530

    Updates

---
+++ precache-manifest.583ef4e818128876b3a13399b85a2779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1cb0e3954b02eaa47422272bd48f60",
+    "url": "/framer-motion/index.html"
+  },
+  {
+    "revision": "3dced1c8c19bbe83991b",
+    "url": "/framer-motion/static/css/main.e708d897.chunk.css"
+  },
+  {
+    "revision": "ac5afcf79ec8f64e9bc0",
+    "url": "/framer-motion/static/js/2.e000f48b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-motion/static/js/2.e000f48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dced1c8c19bbe83991b",
+    "url": "/framer-motion/static/js/main.bd4b8bc6.chunk.js"
+  },
+  {
+    "revision": "14e8e23d6907bc31359f",
+    "url": "/framer-motion/static/js/runtime-main.c018894c.js"
+  }
+]);
